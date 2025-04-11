8cd3176688ce6aee93a6418aeebffc29695922a9
노영오 <nohyo05@naver.com>
2025-04-11 13:44:37 +0900

    chore: 멀티 모듈 구조 생성

---
+++ zariyo-app/src/test/java/com/zariyoapi/ZariyoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zariyoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZariyoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
