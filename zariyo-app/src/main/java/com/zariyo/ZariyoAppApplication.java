a4b2d3fa74900afb38918fe2b341b571a2208a83
노영오 <nohyo05@naver.com>
2025-04-13 11:12:22 +0900

    chore: 패키지명 변경

--- zariyo-app/src/main/java/com/zariyo/ZariyoAppApplication.java
+++ zariyo-app/src/main/java/com/zariyo/ZariyoAppApplication.java
@@ -1,4 +1,4 @@
-package com.zariyoapi;
+package com.zariyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- zariyo-app/src/test/java/com/zariyo/ZariyoAppApplicationTests.java
+++ zariyo-app/src/test/java/com/zariyo/ZariyoAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zariyoapi;
+package com.zariyo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
