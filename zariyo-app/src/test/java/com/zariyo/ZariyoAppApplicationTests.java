0ba5d8829065d4766c7e52561957b4b531664630
노영오 <nohyo05@naver.com>
2025-04-15 10:09:10 +0900

    test: 현재 불필요한 contextLoads 테스트 제거

--- zariyo-app/src/test/java/com/zariyo/ZariyoAppApplicationTests.java
+++ zariyo-app/src/test/java/com/zariyo/ZariyoAppApplicationTests.java
@@ -1,13 +1,8 @@
 package com.zariyo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ZariyoAppApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
