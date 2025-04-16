e7bd4e67214b5bc3548d2a3921c027080c0a5913
노영오 <nohyo05@naver.com>
2025-04-16 12:29:15 +0900

    chore: 버전 카탈로그 적용, settings.gradle 수정, 대기열 모듈 부트 설정

---
+++ zariyo-queue/src/main/java/com/zariyo/ZariyoQueueApplication.java
@@ -0,0 +1,11 @@
+package com.zariyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZariyoQueueApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZariyoQueueApplication.class, args);
+    }
+}
