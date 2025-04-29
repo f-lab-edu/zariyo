3079e51ab60809d34a0297cc0e90feb161ba6d83
노영오 <nohyo05@naver.com>
2025-04-30 01:21:48 +0900

    chore: redis config 설정

---
+++ zariyo-queue/src/main/java/com/zariyo/config/MainRedisConfig.java
@@ -0,0 +1,25 @@
+package com.zariyo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class MainRedisConfig {
+    @Value("${redis.main.host}")
+    private String redisHost;
+
+    @Value("${redis.main.port}")
+    private int redisPort;
+
+    @Bean(name = "mainRedisTemplate")
+    public StringRedisTemplate mainRedisTemplate() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(config);
+        factory.afterPropertiesSet();
+        return new StringRedisTemplate(factory);
+    }
+}
