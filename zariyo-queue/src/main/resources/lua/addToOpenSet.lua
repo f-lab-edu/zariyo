47c84aeec4ff7df2723e158f620814ac3eda91d4
노영오 <nohyo05@naver.com>
2025-05-09 00:30:47 +0900

    feat(queue): lua script (SADD 토큰 추가) 수정

--- zariyo-queue/src/main/resources/lua/addToOpenSet.lua
+++ zariyo-queue/src/main/resources/lua/addToOpenSet.lua
@@ -2,9 +2,10 @@ local threshold = tonumber(redis.call('GET', KEYS[3]) or '500')
 local currentCount = tonumber(redis.call('GET', KEYS[2]) or '0')
 
 if currentCount < threshold then
-    redis.call('SET', KEYS[1], '1', 'EX', ARGV[1])
+    redis.call('SET', KEYS[1], ARGV[1])
     redis.call('INCRBY', KEYS[2], 1)
-    return 1
+    redis.call('SADD', KEYS[4], ARGV[2])
+    return true
 else
-    return 0
+    return false
 end
