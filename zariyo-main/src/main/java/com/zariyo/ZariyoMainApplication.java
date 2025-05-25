9608eabe2f11565e146958c11498e6254b5e9c7b
노영오 <nohyo05@naver.com>
2025-05-25 21:48:52 +0900

    refactor: 스케줄링 기능 활성화를 위한 EnableScheduling 애노테이션 추가

--- zariyo-main/src/main/java/com/zariyo/ZariyoMainApplication.java
+++ zariyo-main/src/main/java/com/zariyo/ZariyoMainApplication.java
@@ -2,8 +2,10 @@ package com.zariyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ZariyoMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZariyoMainApplication.class, args);
