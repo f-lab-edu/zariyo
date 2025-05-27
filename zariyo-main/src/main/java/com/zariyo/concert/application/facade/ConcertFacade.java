d01cb7bbf9b296ce98039c3c3d3632ba671cc60c
노영오 <nohyo05@naver.com>
2025-05-27 23:07:34 +0900

    refactor: Facade 스케줄별 좌석 조회 기능 및 캐시 기능 제거

--- zariyo-main/src/main/java/com/zariyo/concert/application/facade/ConcertFacade.java
+++ zariyo-main/src/main/java/com/zariyo/concert/application/facade/ConcertFacade.java
@@ -1,20 +1,25 @@
 package com.zariyo.concert.application.facade;
 
+import com.zariyo.concert.api.response.AvailableSeatsResponse;
 import com.zariyo.concert.api.response.ConcertDetailResponse;
 import com.zariyo.concert.api.response.ConcertListResponse;
-import com.zariyo.concert.application.cache.ConcertCacheManager;
+import com.zariyo.concert.application.dto.AvailableSeatsDto;
+import com.zariyo.concert.application.dto.SeatInfoDto;
 import com.zariyo.concert.application.service.ConcertQueryService;
+import com.zariyo.concert.application.service.ScheduleSeatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class ConcertFacade {
 
     private final ConcertQueryService concertQueryService;
-    private final ConcertCacheManager cacheManager;
+    private final ScheduleSeatService scheduleSeatService;
 
     public ConcertListResponse getConcerts(Pageable pageable, String sortType, Long categoryId) {
         Pageable sortedPageable = PageRequest.of(
@@ -22,32 +27,16 @@ public class ConcertFacade {
                 pageable.getPageSize(),
                 SortType.fromValue(sortType)
         );
-
-        String cacheKey = cacheManager.generateCacheKey(categoryId, sortType,
-                sortedPageable.getPageNumber(), sortedPageable.getPageSize());
-
-        ConcertListResponse cachedResponse = cacheManager.get(cacheKey);
-        if (cachedResponse != null) {
-            return cachedResponse;
-        }
-
-        ConcertListResponse response = ConcertListResponse.from(concertQueryService.getConcerts(sortedPageable, categoryId));
-        cacheManager.put(cacheKey, response);
-
-        return response;
+        return ConcertListResponse.from(concertQueryService.getConcerts(sortedPageable, categoryId));
     }
 
     public ConcertDetailResponse getConcertDetail(Long concertId) {
-        String cacheKey = cacheManager.generateDetailCacheKey(concertId);
+        return ConcertDetailResponse.from(concertQueryService.getConcertDetail(concertId));
+    }
 
-        ConcertDetailResponse cachedResponse = cacheManager.getDetail(cacheKey);
-        if (cachedResponse != null) {
-            return cachedResponse;
-        }
-
-        ConcertDetailResponse response = ConcertDetailResponse.from(concertQueryService.getConcertDetail(concertId));
-        cacheManager.putDetail(cacheKey, response);
-
-        return response;
+    public AvailableSeatsResponse getAvailableSeats(Long scheduleId) {
+        AvailableSeatsDto availableSeatsdto = scheduleSeatService.getAvailableSeats(scheduleId);
+        List<SeatInfoDto> allSeats = scheduleSeatService.getAllSeats(scheduleId);
+        return AvailableSeatsResponse.from(availableSeatsdto, allSeats);
     }
 }
