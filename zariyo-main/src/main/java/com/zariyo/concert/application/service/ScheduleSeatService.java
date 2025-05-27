2d59b531208478ec00ebca7348e8566ba1af38bf
노영오 <nohyo05@naver.com>
2025-05-27 23:11:35 +0900

    feat: 스케줄별 좌석조회 Service 추가 및 @Cacheable 기능 추가

---
+++ zariyo-main/src/main/java/com/zariyo/concert/application/service/ScheduleSeatService.java
@@ -0,0 +1,60 @@
+package com.zariyo.concert.application.service;
+
+import com.zariyo.concert.application.dto.AvailableSeatsDto;
+import com.zariyo.concert.application.dto.SeatInfoDto;
+import com.zariyo.concert.domain.repository.ScheduleSeatRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ScheduleSeatService {
+
+    private final ScheduleSeatRepository scheduleSeatRepository;
+
+    @Cacheable(
+        value = "available-seats",
+        key = "#scheduleId"
+    )
+    public AvailableSeatsDto getAvailableSeats(Long scheduleId) {
+        return AvailableSeatsDto.builder()
+                .scheduleId(scheduleId)
+                .availableSeats(
+                        scheduleSeatRepository.findAvailableSeatsByScheduleId(scheduleId).stream()
+                        .map(scheduleSeat -> SeatInfoDto.builder()
+                                .scheduleSeatId(scheduleSeat.getScheduleSeatId())
+                                .seatGrade(scheduleSeat.getSeatGrade())
+                                .block(scheduleSeat.getHallSeat().getBlock())
+                                .seatRow(scheduleSeat.getHallSeat().getSeatRow())
+                                .seatNumber(scheduleSeat.getHallSeat().getSeatNumber())
+                                .price(scheduleSeat.getPrice())
+                                .status(scheduleSeat.getStatus().name())
+                                .build())
+                        .toList()
+                )
+                .build();
+    }
+
+    @Cacheable(
+        value = "all-seats",
+        key = "#scheduleId"
+    )
+    public List<SeatInfoDto> getAllSeats(Long scheduleId) {
+        return scheduleSeatRepository.findAllSeatsByScheduleId(scheduleId).stream()
+                .map(scheduleSeat -> SeatInfoDto.builder()
+                        .scheduleSeatId(scheduleSeat.getScheduleSeatId())
+                        .seatGrade(scheduleSeat.getSeatGrade())
+                        .block(scheduleSeat.getHallSeat().getBlock())
+                        .seatRow(scheduleSeat.getHallSeat().getSeatRow())
+                        .seatNumber(scheduleSeat.getHallSeat().getSeatNumber())
+                        .price(scheduleSeat.getPrice())
+                        .status(scheduleSeat.getStatus().name())
+                        .build())
+                .toList();
+    }
+}
