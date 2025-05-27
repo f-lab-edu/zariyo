de3efd34ed3bb32e2a92dac7fcd448204291a397
노영오 <nohyo05@naver.com>
2025-05-27 22:58:21 +0900

    refactor: RedisConnectionFactory 분리로 중복 제거 및 재사용성 향상

--- zariyo-main/src/main/java/com/zariyo/common/config/MainRedisConfig.java
+++ zariyo-main/src/main/java/com/zariyo/common/config/MainRedisConfig.java
@@ -1,49 +1,28 @@
 package com.zariyo.common.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class MainRedisConfig {
+
     @Value("${redis.main.host}")
     private String redisHost;
 
     @Value("${redis.main.port}")
     private int redisPort;
 
+    @Bean(name = "mainRedisConnectionFactory")
+    public LettuceConnectionFactory mainRedisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
+    }
+
     @Bean(name = "mainRedisTemplate")
     public StringRedisTemplate mainRedisTemplate() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(config);
-        factory.afterPropertiesSet();
-        return new StringRedisTemplate(factory);
-    }
-    
-    @Bean(name = "mainRedisObjectTemplate")
-    public RedisTemplate<String, Object> mainRedisObjectTemplate() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(config);
-        factory.afterPropertiesSet();
-        
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
-        
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
-        
-        template.afterPropertiesSet();
-        return template;
+        return new StringRedisTemplate(mainRedisConnectionFactory());
     }
 }
