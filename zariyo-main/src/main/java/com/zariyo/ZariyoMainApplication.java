098f73e09327e9ef2d9991922b6bc799fc9f1f16
노영오 <nohyo05@naver.com>
2025-06-09 22:43:55 +0900

    refactor: EnableAsync 애노테이션 제거

--- zariyo-main/src/main/java/com/zariyo/ZariyoMainApplication.java
+++ zariyo-main/src/main/java/com/zariyo/ZariyoMainApplication.java
@@ -2,12 +2,10 @@ package com.zariyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync
 public class ZariyoMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZariyoMainApplication.class, args);
